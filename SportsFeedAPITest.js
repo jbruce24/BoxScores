6c140e90c3bd1b9cdc86a4a935fb3e14a9dfb2d7
Jared Bruce <jared.bruce24@gmail.com>
2019-02-08 00:38:11 -0600

    Add files via upload
    Updated to only pull in games vs certain opponents.

--- SportsFeedAPITest.js
+++ SportsFeedAPITest.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $.ajax
   ({
     type: "GET",
-    url: 'https://api.mysportsfeeds.com/v2.0/pull/mlb/2018-regular/date/20180730/games.json',
+    url: 'https://api.mysportsfeeds.com/v2.0/pull/mlb/2018-regular/games.json?team=111',
     dataType: 'json',
     async: false,
     headers: {
@@ -12,12 +12,31 @@ $(document).ready(function() {
     data: {
             format: 'json'
           },
-    success: function(response){
+    success: function(response)
+    {
 
-      var games = response.games[0].schedule.id;
+
+    function game(i)
+      {
+      var games = response.games[i].schedule.id;
+      var teamId = response.games[i].schedule;
+      var awayAb = teamId.awayTeam.abbreviation;
+      var homeAb = teamId.homeTeam.abbreviation;
+      var awayId = teamId.awayTeam.id;
+      var homeId = teamId.homeTeam.id;
+        if(awayId == 120 || homeId == 120)
+          {
+            return $('.todaysGames').append(`<p>${games} and home ${homeId} ${homeAb} and away ${awayId} ${awayAb} ${i}</p>`);
+          };
       //var gamesId = games.schedule.id;
       //console.log('Jared');
-      return $('.todaysGames').append(`<p>${games}</p>`);
-    }
+      };
+      for(i=0; i<response.games.length; i++)
+        {
+
+          game(i);
+        };
+
+     }
   });
 });
